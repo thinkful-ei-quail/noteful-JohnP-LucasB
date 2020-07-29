9740d26576c3d609abf6a2a5132b323a6311455b
John Pendergast <jpending@gmail.com>
2020-07-29 15:45:40 -0400

    first stab at implementing context

---
+++ src/Context.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default React.createContext(
+    { 
+        notes: [], 
+        folders: [], 
+        addFolder: () => {}, 
+        addNote: () => {}, 
+        deleteNote: () => {}, 
+    }
+)
+
