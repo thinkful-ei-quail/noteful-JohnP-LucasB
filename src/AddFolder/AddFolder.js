b038b484ea9018919f0b1fc6bc67d80346b9aaf7
Lucas Bigelow <lukejbigelow@gmail.com>
2020-07-30 12:26:05 -0500

    fixed up add folder post

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -21,7 +21,7 @@ handleAddFolder(evt) {
 
     const BASE_URL = 'http://localhost:9090/folders'
     
-    const options = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(this.state.folderName)}
+    const options = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({name: this.state.folderName})}
     
     fetch(BASE_URL, options)
         .then(res => res.ok ? res.json() : console.log('YOU FUCKED IT ALL UP GODDAMNIT'))
